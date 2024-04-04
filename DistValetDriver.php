faad0b0d41ba38e201ea311fb29fb7b441fe27d7
Gkiokan Sali <gkiokan@hotmail.de>
2024-04-04 17:05:47 +0200

    Adding proper fallback to handle not existing paths

--- DistValetDriver.php
+++ DistValetDriver.php
@@ -16,6 +16,7 @@ class DistValetDriver extends BasicValetDriver
      */
     public function serves(string $sitePath, string $siteName, string $uri): bool
     {
+
         return file_exists($sitePath . '/dist/index.html');
     }
 
@@ -30,8 +31,13 @@ class DistValetDriver extends BasicValetDriver
     public function isStaticFile(string $sitePath, string $siteName, string $uri)/*: string|false */
     {
         if (file_exists($staticFilePath = $sitePath . '/dist' . $uri)) {
-            return $staticFilePath;
+            if( is_file($staticFilePath) ) 
+                return $staticFilePath;
+
+            if( file_exists($staticFilePath = $staticFilePath . '/index.html') )
+                return $staticFilePath;        
         }
+
         return false;
     }
 
@@ -45,6 +51,19 @@ class DistValetDriver extends BasicValetDriver
      */
     public function frontControllerPath(string $sitePath, string $siteName, string $uri): ?string
     {
-        return $sitePath . '/dist/index.html';
+        // Enable this if you just want to have straight dist/index.html to be served by default
+        // Issue with this is that 'not existing paths' will still show up the default page.
+        // return $sitePath . '/dist/index.html';        
+         
+        // Path the $uri and make sure we have a build file at that path that should be resolved
+        if( $uri == '/' )
+            $uri = '';
+
+        if( file_exists($fullPath = $sitePath . '/dist' . $uri . '/index.html') )
+            return $fullPath;
+
+        // In case the file doesn't exists, it just returns 404 by valet
+        // This could be optimized to allow $sitePath/dist/404.html or something
+        return false;
     }
 }
